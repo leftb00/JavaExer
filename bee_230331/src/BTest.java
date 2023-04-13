73a51ad71e8fc9996e8d2c03ac7f9ddc8f7d27d6
JBEDU03 <JBEDU03@DESKTOP-HMF1SKG>
2023-04-14 08:38:06 +0900

    ...

--- bee_230331/src/BTest.java
+++ bee_230331/src/BTest.java
@@ -1,9 +1,8 @@
 
-public class BTest {
-
-	public static void main(String[] args) {
+public class BTest
+{
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-
 	}
-
 }
